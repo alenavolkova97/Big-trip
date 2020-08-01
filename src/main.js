90dc6808bf7d688712cf19fad00595967679b8db
Alena Shashko <L.Shashko@mail.ru>
2020-08-01 11:59:17 +0300

    render site menu and trip events filter

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@
 
 const headerElement = document.querySelector(`.page-header`);
 const tripInfoContainerElement = headerElement.querySelector(`.trip-main`);
+const tripControlsContainerElement = tripInfoContainerElement.querySelector(`.trip-controls`);
+const siteMenuHeaderElement = tripControlsContainerElement.querySelector(`h2:nth-child(1)`);
+const tripEventsFilterHeaderElement = tripControlsContainerElement.querySelector(`h2:nth-child(2)`);
 
 const createTripInfoTemplate = () => {
   return (
@@ -19,9 +22,42 @@ const createTripInfoTemplate = () => {
   );
 };
 
+const createSiteMenuTemplate = () => {
+  return (
+    `<nav class="trip-controls__trip-tabs  trip-tabs">
+      <a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Table</a>
+      <a class="trip-tabs__btn" href="#">Stats</a>
+    </nav>`
+  );
+};
+
+const createTripEventsFilterTemplate = () => {
+  return (
+    `<form class="trip-filters" action="#" method="get">
+      <div class="trip-filters__filter">
+        <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked>
+        <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
+      </div>
+
+      <div class="trip-filters__filter">
+        <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
+        <label class="trip-filters__filter-label" for="filter-future">Future</label>
+      </div>
+
+      <div class="trip-filters__filter">
+        <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past">
+        <label class="trip-filters__filter-label" for="filter-past">Past</label>
+      </div>
+
+      <button class="visually-hidden" type="submit">Accept filter</button>
+    </form>`
+  );
+};
+
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
 render(tripInfoContainerElement, createTripInfoTemplate(), `afterbegin`);
-
+render(siteMenuHeaderElement, createSiteMenuTemplate(), `afterend`);
+render(tripEventsFilterHeaderElement, createTripEventsFilterTemplate(), `afterend`);
