32f39403d57ad64819b6e99732a54b9b1fb7deae
Alena Shashko <L.Shashko@mail.ru>
2020-08-03 00:02:26 +0300

    render trip events sorting

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ const headerContainerElement = headerElement.querySelector(`.trip-main`);
 const tripControlsContainerElement = headerContainerElement.querySelector(`.trip-controls`);
 const siteMenuHeaderElement = tripControlsContainerElement.querySelector(`h2:nth-child(1)`);
 const tripEventsFilterHeaderElement = tripControlsContainerElement.querySelector(`h2:nth-child(2)`);
+const mainElement = document.querySelector(`main`);
+const tripEventsContainerElement = mainElement.querySelector(`.trip-events`);
 
 const createTripInfoContainerTemplate = () => {
   return (
@@ -63,6 +65,41 @@ const createTripEventsFilterTemplate = () => {
   );
 };
 
+const createTripEventsSorting = () => {
+  return (
+    `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
+      <span class="trip-sort__item  trip-sort__item--day">Day</span>
+
+      <div class="trip-sort__item  trip-sort__item--event">
+        <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-event" checked>
+        <label class="trip-sort__btn" for="sort-event">Event</label>
+      </div>
+
+      <div class="trip-sort__item  trip-sort__item--time">
+        <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">
+        <label class="trip-sort__btn" for="sort-time">
+          Time
+          <svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
+            <path d="M2.888 4.852V9.694H5.588V4.852L7.91 5.068L4.238 0.00999987L0.548 5.068L2.888 4.852Z"/>
+          </svg>
+        </label>
+      </div>
+
+      <div class="trip-sort__item  trip-sort__item--price">
+        <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price">
+        <label class="trip-sort__btn" for="sort-price">
+          Price
+          <svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
+            <path d="M2.888 4.852V9.694H5.588V4.852L7.91 5.068L4.238 0.00999987L0.548 5.068L2.888 4.852Z"/>
+          </svg>
+        </label>
+      </div>
+
+      <span class="trip-sort__item  trip-sort__item--offers">Offers</span>
+    </form>`
+  );
+};
+
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
@@ -75,3 +112,4 @@ render(tripInfoContainerElement, createTripInfoTemplate(), `beforeend`);
 render(tripInfoContainerElement, createTripPriceTemplate(), `beforeend`);
 render(siteMenuHeaderElement, createSiteMenuTemplate(), `afterend`);
 render(tripEventsFilterHeaderElement, createTripEventsFilterTemplate(), `afterend`);
+render(tripEventsContainerElement, createTripEventsSorting(), `beforeend`);
