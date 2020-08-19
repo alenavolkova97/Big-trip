db3f11aa1da899ce960c505cbdf2652ea30908d8
Alena Shashko <L.Shashko@mail.ru>
2020-08-19 03:21:46 +0300

    finish with adding data to event-edit template

--- src/view/trip-event-edit.js
+++ src/view/trip-event-edit.js
@@ -25,19 +25,38 @@ const createTripDestinationSelectTemplate = () => {
     `<option value="${destination}"></option>`).join(``);
 };
 
-const createTripOffersTemplate = (offers) => {
+const createTripEventTimeTemplate = (time) => {
+  return (
+    `<div class="event__field-group  event__field-group--time">
+      <label class="visually-hidden" for="event-start-time">
+        From
+      </label>
+      <input class="event__input  event__input--time" id="event-start-time"
+        type="text" name="event-start-time" value="${time.start}">
+      &mdash;
+      <label class="visually-hidden" for="event-end-time">
+        To
+      </label>
+      <input class="event__input  event__input--time" id="event-end-time"
+        type="text" name="event-end-time" value="${time.end}">
+    </div>`
+  );
+};
+
+const createTripEventOffersTemplate = (offers) => {
   return offers.map((offer) =>
     `<div class="event__offer-selector">
-      <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage" type="checkbox" name="event-offer-luggage" checked>
-      <label class="event__offer-label" for="event-offer-luggage">
-        <span class="event__offer-title">Add luggage</span>
+      <input class="event__offer-checkbox  visually-hidden" id="event-offer-${offer.key}"
+        type="checkbox" name="event-offer-${offer.key}" ${offer.isChecked ? `checked` : ``}>
+      <label class="event__offer-label" for="event-offer-${offer.key}">
+        <span class="event__offer-title">${offer.title}</span>
         &plus;
-        &euro;&nbsp;<span class="event__offer-price">30</span>
+        &euro;&nbsp;<span class="event__offer-price">${offer.price}</span>
       </label>
     </div>`).join(``);
 };
 
-const createTripPhotosTemplate = (photos) => {
+const createTripEventPhotosTemplate = (photos) => {
   return photos.map((photo) =>
     `<img class="event__photo" src="${photo}" alt="Event photo">`).join(``);
 };
@@ -52,7 +71,8 @@ export const createTripEventEditTemplate = (tripEvent = {}) => {
       end: `18/03/19 00:00`
     },
     price = ``,
-    offers = OFFERS, // offers list (in case of create new event) will be received from the server
+    offers = OFFERS,
+    // offers list (in case of create new event) will be received from the server
     description = `Geneva is a city in Switzerland that lies at
       the southern tip of expansive Lac Léman (Lake Geneva).
       Surrounded by the Alps and Jura mountains,
@@ -68,7 +88,8 @@ export const createTripEventEditTemplate = (tripEvent = {}) => {
         <div class="event__type-wrapper">
           <label class="event__type  event__type-btn" for="event-type-toggle">
             <span class="visually-hidden">Choose event type</span>
-            <img class="event__type-icon" width="17" height="17" src="img/icons/${type.toLowerCase()}.png" alt="Event type icon">
+            <img class="event__type-icon" width="17" height="17"
+              src="img/icons/${type.toLowerCase()}.png" alt="Event type icon">
           </label>
           <input class="event__type-toggle  visually-hidden" id="event-type-toggle" type="checkbox">
 
@@ -89,23 +110,14 @@ export const createTripEventEditTemplate = (tripEvent = {}) => {
           <label class="event__label  event__type-output" for="event-destination">
             ${type} ${ARRIVALS.includes(type) ? `in` : `to`}
           </label>
-          <input class="event__input  event__input--destination" id="event-destination" type="text" name="event-destination" value="${destination}" list="destination-list">
+          <input class="event__input  event__input--destination" id="event-destination"
+            type="text" name="event-destination" value="${destination}" list="destination-list">
           <datalist id="destination-list">
             ${createTripDestinationSelectTemplate()}
           </datalist>
         </div>
 
-        <div class="event__field-group  event__field-group--time">
-          <label class="visually-hidden" for="event-start-time">
-            From
-          </label>
-          <input class="event__input  event__input--time" id="event-start-time" type="text" name="event-start-time" value="${time.start}">
-          &mdash;
-          <label class="visually-hidden" for="event-end-time">
-            To
-          </label>
-          <input class="event__input  event__input--time" id="event-end-time" type="text" name="event-end-time" value="${time.end}">
-        </div>
+        ${createTripEventTimeTemplate(time)}
 
         <div class="event__field-group  event__field-group--price">
           <label class="event__label" for="event-price">
@@ -121,16 +133,8 @@ export const createTripEventEditTemplate = (tripEvent = {}) => {
       <section class="event__details">
         <section class="event__section  event__section--offers">
           <h3 class="event__section-title  event__section-title--offers">Offers</h3>
-
           <div class="event__available-offers">
-            <div class="event__offer-selector">
-              <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage" type="checkbox" name="event-offer-luggage" checked>
-              <label class="event__offer-label" for="event-offer-luggage">
-                <span class="event__offer-title">Add luggage</span>
-                &plus;
-                &euro;&nbsp;<span class="event__offer-price">30</span>
-              </label>
-            </div>
+            ${createTripEventOffersTemplate(offers)}
           </div>
         </section>
 
@@ -140,7 +144,7 @@ export const createTripEventEditTemplate = (tripEvent = {}) => {
 
           <div class="event__photos-container">
             <div class="event__photos-tape">
-              ${createTripPhotosTemplate(photos)}
+              ${createTripEventPhotosTemplate(photos)}
             </div>
           </div>
         </section>
