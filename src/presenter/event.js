873dcd9832836568f7b78b0d7f0c340ab36bd53e
Alena Shashko <L.Shashko@mail.ru>
2020-09-15 22:33:35 +0300

    expand events.js presenter

--- src/presenter/event.js
+++ src/presenter/event.js
@@ -1,6 +1,6 @@
 import TripEventView from '../view/trip-event.js';
 import TripEventEditView from '../view/trip-event-edit.js';
-import {render, replace} from '../utils/render.js';
+import {render, replace, remove} from '../utils/render.js';
 
 export default class Event {
   constructor(eventListContainer) {
@@ -17,10 +17,36 @@ export default class Event {
   init(event) {
     this._event = event;
 
+    const prevTripEventComponent = this._tripEventComponent;
+    const prevTripEventEditComponent = this._tripEventEditComponent;
+
     this._tripEventComponent = new TripEventView(event);
 
     this._tripEventComponent.setRollupClickHandler(this._handleRollupClick);
-    render(this._eventListContainer, this._tripEventComponent);
+
+    if (prevTripEventComponent === null || prevTripEventEditComponent === null) {
+      render(this._eventListContainer, this._tripEventComponent);
+      return;
+    }
+
+    if (this._eventListContainer.getElement().contains(prevTripEventComponent.getElement())) {
+      replace(this._tripEventComponent, prevTripEventComponent);
+    }
+
+    if (this._eventListContainer.getElement().contains(prevTripEventEditComponent.getElement())) {
+      replace(this._tripEventEditComponent, prevTripEventEditComponent);
+    }
+
+    remove(prevTripEventComponent);
+
+    if (prevTripEventEditComponent) {
+      remove(prevTripEventEditComponent);
+    }
+  }
+
+  destroy() {
+    remove(this._tripEventComponent);
+    remove(this._tripEventEditComponent);
   }
 
   _replaceEventToForm() {
