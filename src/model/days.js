98b1e28fb2819ef63bd6e421b7a2dd5fae9dd712
Alena Shashko <L.Shashko@mail.ru>
2020-09-26 15:29:28 +0300

    fix days' serial number

--- src/model/days.js
+++ src/model/days.js
@@ -25,17 +25,8 @@ export default class Days extends Observer {
     this._days = newDays;
   }
 
-  _updateDay(day, dayIndex) {
-    this._days = [
-      ...this._days.slice(0, dayIndex),
-      day,
-      ...this._days.slice(dayIndex + 1)
-    ];
-  }
-
   updateEvent(updateType, updateEvent) {
     let dayContainUpdateEvent;
-    let dayContainUpdateEventIndex;
     let updateEventIndex;
 
     for (let dayIndex = 0; dayIndex < this._days.length; dayIndex++) {
@@ -43,7 +34,6 @@ export default class Days extends Observer {
       const eventIndex = day.tripEvents.findIndex((event) => updateEvent.id === event.id);
       if (eventIndex !== -1) {
         dayContainUpdateEvent = day;
-        dayContainUpdateEventIndex = dayIndex;
         updateEventIndex = eventIndex;
         break;
       }
@@ -74,7 +64,6 @@ export default class Days extends Observer {
 
   deleteEvent(updateType, deleteEvent) {
     let dayContainDeleteEvent;
-    let dayContainDeleteEventIndex;
     let deleteEventIndex;
 
     for (let dayIndex = 0; dayIndex < this._days.length; dayIndex++) {
@@ -82,7 +71,6 @@ export default class Days extends Observer {
       const eventIndex = day.tripEvents.findIndex((event) => deleteEvent.id === event.id);
       if (eventIndex !== -1) {
         dayContainDeleteEvent = day;
-        dayContainDeleteEventIndex = dayIndex;
         deleteEventIndex = eventIndex;
         break;
       }
@@ -97,7 +85,7 @@ export default class Days extends Observer {
       ...dayContainDeleteEvent.tripEvents.slice(deleteEventIndex + 1)
     ];
 
-    this._updateDay(dayContainDeleteEvent, dayContainDeleteEventIndex);
+    this._updateDays(groupEventsByDays(this.getAllEvents()));
 
     this._notify(updateType);
   }
